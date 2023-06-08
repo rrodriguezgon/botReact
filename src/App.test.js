3b819c717b1c1ea2f367314d08f91f2451316a26
Raquel Rodriguez Gonzalez <raquel.rodriguezgonzalez@gruposantander.com>
2023-06-08 12:23:18 +0200

    añadidos primeros tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders component Navbar', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('BOT MARCADOR');
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders component Routes', () => {
+	render(<App />);
+	const linkElement = screen.getByText('HOME');
+	expect(linkElement).toBeInTheDocument();
+  });
