13027f3823d71b2648502b364038223222574cab
DESKTOP-S98238J\Raquel <Raquel Rodriguez Gonzalez>
2024-02-17 00:05:30 +0100

    configurar urls

--- src/services/logs.js
+++ src/services/logs.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const baseURL = "http://217.182.129.103:4000/api/v1/logs";
-const baseURL = "http://localhost:4000/api/v1/logs";
+const baseURL = "http://217.182.129.103:4000/api/v1/logs";
+// const baseURL = "http://localhost:4000/api/v1/logs";
 
 export const getAll = () =>
   axios.get(baseURL).then((response) => {
